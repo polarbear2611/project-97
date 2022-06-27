cb92b2f7b59ac038fb68dfcbdb9bbfc9c9ee58ea
polarbear2611 <98675499+polarbear2611@users.noreply.github.com>
2022-06-27 20:32:09 +1000

    Add files via upload

---
+++ converter.js
@@ -0,0 +1,39 @@
+function convert1()
+{
+    var convert = document.getElementById("input_convert_1").value;
+    var converted = convert / 10;
+    document.getElementById("convert1_answer").innerHTML = converted + " cm";
+
+}
+function convert2()
+{
+    var convert = document.getElementById("input_convert_2").value;
+    var converted = convert / 100;
+    document.getElementById("convert2_answer").innerHTML = converted + " m";
+
+}
+function convert3()
+{
+    var convert = document.getElementById("input_convert_3").value;
+    var converted = convert / 1000;
+    document.getElementById("convert3_answer").innerHTML = converted + " km";
+
+}
+function convert4()
+{
+    var convert = document.getElementById("input_convert_4").value;
+    var converted = convert / 1000;
+    document.getElementById("convert4_answer").innerHTML = converted + " L";
+} 
+function convert5()
+{
+    var convert = document.getElementById("input_convert_5").value;
+    var converted = convert / 1000;
+    document.getElementById("convert5_answer").innerHTML = converted + " g";
+} 
+function convert6()
+{
+    var convert = document.getElementById("input_convert_6").value;
+    var converted = convert / 1000;
+    document.getElementById("convert6_answer").innerHTML = converted + " kg";
+} 
---
+++ main.js
@@ -0,0 +1,12 @@
+function convert()
+{
+    window.location = "converter.html";
+}
+function formal()
+{
+    window.location = "formal.html";
+}
+function calculator()
+{
+    window.location = "calculator.html";
+}
